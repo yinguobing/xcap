a30c45facb3938c35a9df33535e807a6284656d6
yinguobing <yinguobing@gmail.com>
2024-08-06 17:02:57 +0800

    支持分割mcap文件提取

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
+use std::collections::HashMap;
 use std::fs;
 use std::io::Read;
 use std::path::PathBuf;
@@ -31,6 +32,69 @@ struct Args {
     verbose: bool,
 }
 
+struct Topic {
+    id: u16,
+    name: String,
+    description: String,
+    msg_count: u64,
+}
+
+impl std::fmt::Display for Topic {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "id: {}, {}, msgs: {}, {}",
+            self.id, self.name, self.msg_count, self.description
+        )
+    }
+}
+
+fn summary(files: &Vec<PathBuf>) -> Vec<Topic> {
+    let mut topics: HashMap<u16, Topic> = HashMap::new();
+
+    for file in files {
+        let mut fd = fs::File::open(file).unwrap();
+        let mut buf = Vec::new();
+        fd.read_to_end(&mut buf).unwrap();
+        let Some(summary) = mcap::read::Summary::read(&buf).unwrap() else {
+            panic!("Failed to read summary info: {}", file.display());
+        };
+
+        // Statistics
+        let Some(stats) = summary.stats else {
+            panic!("Failed to get statistics: {}", file.display())
+        };
+
+        // Topics
+        let channels: Vec<_> = summary.channels.into_iter().collect();
+        for chn in channels {
+            topics
+                .entry(chn.0)
+                .and_modify(|t| {
+                    t.id = chn.0;
+                    t.name = chn.1.topic.clone();
+                    t.description = format!(
+                        "MsgType: {}, Encoding: {}",
+                        chn.1.schema.as_ref().unwrap().name,
+                        chn.1.schema.as_ref().unwrap().encoding
+                    );
+                    t.msg_count += stats.channel_message_counts.get(&chn.0).unwrap();
+                })
+                .or_insert(Topic {
+                    id: chn.0,
+                    name: chn.1.topic.clone(),
+                    description: format!(
+                        "MsgType: {}, Encoding: {}",
+                        chn.1.schema.as_ref().unwrap().name,
+                        chn.1.schema.as_ref().unwrap().encoding
+                    ),
+                    msg_count: stats.channel_message_counts.get(&chn.0).unwrap().clone(),
+                });
+        }
+    }
+    topics.into_values().collect()
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -41,45 +105,24 @@ fn main() {
     }
 
     // Find all macp files
+    println!("Reading direcory...");
     let mut files: Vec<PathBuf> = fs::read_dir(&args.input)
         .unwrap()
         .map(|f| f.unwrap().path())
-        .filter(|f| f.ends_with(".mcap"))
+        .filter(|f| f.is_file() && f.extension().is_some_and(|f| f.eq("mcap")))
         .collect();
     files.sort();
-    println!("Found {} files.", files.len());
-
-    // Summary this file
-    println!("File: {:?}", args.input.as_os_str());
-    let mut fd = fs::File::open(&args.input).unwrap();
-    let mut buf = Vec::new();
-    fd.read_to_end(&mut buf).unwrap();
-    let Some(summary) = mcap::read::Summary::read(&buf).unwrap() else {
-        panic!(
-            "Failed to read summary info: {}",
-            args.input.to_str().unwrap()
-        );
-    };
-
-    // Statistics
-    if let Some(stats) = summary.stats {
-        println!("Messages: {}", stats.message_count);
-    } else {
-        println!("Failed to get statistics.")
+    println!("Found MCAP files: {}", files.len());
+    for f in files.iter() {
+        println!("- {}", f.display());
     }
 
-    // Topics
-    println!("Topics:");
-    let mut channels: Vec<_> = summary.channels.into_iter().collect();
-    channels.sort_by_key(|k| k.0);
-    for chn in channels {
-        println!(
-            " {}, {}, MsgType: {}, Encoding: {}",
-            chn.0,
-            chn.1.topic,
-            chn.1.schema.as_ref().unwrap().name,
-            chn.1.schema.as_ref().unwrap().encoding
-        );
+    // Summary these files
+    let mut topics = summary(&files);
+    topics.sort_by_key(|k| k.id);
+    println!("Found topics: {}", topics.len());
+    for topic in topics.iter() {
+        println!("- {}", topic);
     }
 
     // Setup a progress bar.
@@ -92,7 +135,8 @@ fn main() {
 
     // Any extracting jobs?
     if let Some(topic) = args.topic {
-        // Create output directory
+        println!("Extracting: {}", topic);
+        // Create output directory. Using topic name as directory path.
         let mut output_dir = args
             .output_dir
             .clone()
@@ -103,25 +147,29 @@ fn main() {
             println!("Failed to create output directory: {:?}", output_dir);
             return;
         };
-        println!("Output directory: {:}", output_dir.display());
+        println!("- Output directory: {:}", output_dir.display());
 
-        println!("Processing...");
+        println!("- Extracing H.264...");
         let mut parser = h264::Parser::new(&output_dir);
-        // Gather objects of interests
-        let stream = mcap::MessageStream::new(&buf)
-            .unwrap()
-            .filter(|x| x.as_ref().is_ok_and(|x| x.channel.topic == topic));
         let mut counter = 0;
-        for message in stream {
-            let message = message.unwrap();
-            parser.accumulate(&message);
-            counter += 1;
-            bar.set_message(format!("{} {}", topic, counter.to_string()));
+        for file in files.iter() {
+            let mut fd = fs::File::open(file).unwrap();
+            let mut buf = Vec::new();
+            fd.read_to_end(&mut buf).unwrap();
+            let stream = mcap::MessageStream::new(&buf)
+                .unwrap()
+                .filter(|x| x.as_ref().is_ok_and(|x| x.channel.topic == topic));
+            for message in stream {
+                let message = message.unwrap();
+                parser.accumulate(&message);
+                counter += 1;
+                bar.set_message(format!("{} {}", topic, counter.to_string()));
+            }
         }
-        bar.set_message(format!("{} {} done.", topic, counter.to_string()));
         bar.finish();
 
         // Dump frames
+        println!("- Extracing camera frames...");
         parser.dump_frames();
     } else {
         println!("No topics specified.");
