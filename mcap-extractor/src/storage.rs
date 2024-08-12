634e0685d8bf32d0c513dbc2da69a8a7b2d32443
yinguobing <yinguobing@gmail.com>
2024-08-12 18:55:34 +0800

    support file download from minio

--- mcap-extractor/src/storage.rs
+++ mcap-extractor/src/storage.rs
@@ -1,65 +1,73 @@
-use log::info;
+use log::error;
 use minio::s3::{
-    args::{BucketExistsArgs, MakeBucketArgs, ObjectConditionalReadArgs},
-    client::ClientBuilder,
+    args::{BucketExistsArgs, ObjectConditionalReadArgs},
+    client::{Client, ClientBuilder},
     creds::StaticProvider,
     http::BaseUrl,
 };
-use std::path::Path;
+use std::{fs, path::PathBuf};
+use thiserror::Error;
 
-pub async fn download(
-    base_url: &str,
-    bucket: &str,
-    path: &str,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let base_url = "https://play.min.io".parse::<BaseUrl>()?;
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("S3 error: {0}")]
+    S3Error(#[from] minio::s3::error::Error),
+    #[error("Reqwest error: {0}")]
+    RequestError(#[from] reqwest::Error),
+    #[error("Disk IO error: {0}")]
+    DiskError(#[from] std::io::Error),
+    #[error("Not existed: {0}")]
+    NotExisted(String),
+    #[error("unknown error")]
+    Unknown,
+}
 
-    info!("Trying to connect: `{:?}`", base_url);
+#[derive(Debug)]
+pub struct Agent {
+    client: Client,
+}
 
-    let static_provider = StaticProvider::new(
-        "Q3AM3UQ867SPQQA43P2F",
-        "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
-        None,
-    );
-
-    let client = ClientBuilder::new(base_url.clone())
-        .provider(Some(Box::new(static_provider)))
-        .build()?;
-
-    let bucket_name: &str = "asiatrip";
-
-    // Check 'bucket_name' bucket exist or not.
-    let exists: bool = client
-        .bucket_exists(&BucketExistsArgs::new(&bucket_name).unwrap())
-        .await
-        .unwrap();
-
-    // Make 'bucket_name' bucket if not exist.
-    if !exists {
-        client
-            .make_bucket(&MakeBucketArgs::new(&bucket_name).unwrap())
-            .await
-            .unwrap();
+impl Agent {
+    pub fn new(
+        base_url: &str,
+        region: &str,
+        access_key: &str,
+        secret_key: &str,
+    ) -> Result<Self, Error> {
+        let mut base_url = base_url.parse::<BaseUrl>()?;
+        base_url.region = region.to_string();
+        let static_provider = StaticProvider::new(access_key, secret_key, None);
+        let client = ClientBuilder::new(base_url)
+            .provider(Some(Box::new(static_provider)))
+            .build()?;
+        Ok(Self { client })
     }
 
-    // File we are going to upload to the bucket
-    let filename: &Path = Path::new("/tmp/asiaphotos.zip");
+    pub async fn download_object(
+        &self,
+        bucket: &str,
+        object: &str,
+        local_path: &PathBuf,
+    ) -> Result<(), Error> {
+        // Check bucket exist or not.
+        let exists: bool = self
+            .client
+            .bucket_exists(&BucketExistsArgs::new(&bucket)?)
+            .await?;
+        if !exists {
+            error!("Bucket {} does not exist.", bucket);
+            return Err(Error::NotExisted(bucket.to_string()));
+        }
 
-    // Name of the object that will be stored in the bucket
-    let object_name: &str = "asiaphotos-2015.zip";
-
-    info!("filename {}", &filename.to_str().unwrap());
-
-    let obj_dscp = ObjectConditionalReadArgs::new(bucket_name, object_name).unwrap();
-    let result = client.get_object(&obj_dscp).await?;
-
-    if result.status().is_success() {
-        info!(
-        "file `{}` is successfully downloaded as object `{object_name}` from bucket `{bucket_name}`.",
-        filename.display()
-    );
-    } else {
-        info!("file `{}` download failed.", filename.display());
+        // Download object
+        let obj_dscp = ObjectConditionalReadArgs::new(bucket, object)?;
+        let response = self.client.get_object(&obj_dscp).await?;
+        if response.status().is_success() {
+            fs::write(
+                local_path,
+                response.bytes().await.map_err(|e| Error::RequestError(e))?,
+            )?;
+        }
+        Ok(())
     }
-    Ok(())
 }
