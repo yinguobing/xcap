664b436c5f065cad1b406409124cc6544ae9e333
yinguobing <yinguobing@gmail.com>
2025-03-11 20:01:59 +0800

    support NV12 image format

--- xcap/src/image.rs
+++ xcap/src/image.rs
@@ -59,15 +59,40 @@ impl Extractor for Parser {
             cdr::deserialize_from::<_, Image, _>(serialized.as_slice(), cdr::size::Infinite)
                 .map_err(|e| Error::CDR(e))?;
 
+        if image_msg.encoding != "nv12" {
+            return Ok(());
+        }
+
+        let height_rgb = (image_msg.height as f32 / 1.5) as usize;
+        let mut rgb = vec![0u8; height_rgb * image_msg.width as usize * 3];
+        let _ret = unsafe {
+            libyuv::nv12_to_raw(
+                image_msg.data.as_ptr(),
+                image_msg.width as i32,
+                image_msg
+                    .data
+                    .as_ptr()
+                    .add(image_msg.width as usize * height_rgb),
+                image_msg.width as i32,
+                rgb.as_mut_ptr(),
+                image_msg.width as i32 * 3,
+                image_msg.width as i32,
+                height_rgb as i32,
+            )
+        };
         if let Some(rec) = &self.rec_stream {
             rec.set_time_seconds(
                 "main",
                 image_msg.header.stamp.sec as f64 + image_msg.header.stamp.nanosec as f64 * 1e-9,
             );
-            // rec.log(
-            //     format!("image/{}", message.channel.topic.clone()),
-            //     &rerun::Image::new(image_msg.data),
-            // )?;
+            rec.log(
+                format!("image/{}", message.channel.topic.clone()),
+                &rerun::Image::from_elements(
+                    rgb.as_ref(),
+                    [image_msg.width, height_rgb as u32],
+                    rerun::ColorModel::RGB,
+                ),
+            )?;
         }
 
         // Create output file
@@ -78,6 +103,14 @@ impl Extractor for Parser {
                     .join(format!("{}.bin", message.publish_time)),
             )?;
             file.write_all(&image_msg.data)?;
+            let img = image::RgbImage::from_vec(image_msg.width, height_rgb as u32, rgb)
+                .expect("Image should be valid");
+
+            img.save(
+                &self
+                    .output_dir
+                    .join(format!("{}.jpg", message.publish_time)),
+            )?;
         }
         Ok(())
     }
