4a198df4167ca3411bdcf2f901b48d5ee49591f7
yinguobing <yinguobing@gmail.com>
2025-05-20 20:49:58 +0800

    支持RGB格式的Image

--- xcap/src/parser/image.rs
+++ xcap/src/parser/image.rs
@@ -52,36 +52,39 @@ impl Extractor for Parser {
             cdr::deserialize_from::<_, Image, _>(serialized.as_slice(), cdr::size::Infinite)
                 .map_err(|e| Error::CDR(e))?;
 
-        if image_msg.encoding != "nv12" {
-            return Ok(());
-        }
-
-        let height_rgb = (image_msg.height as f32 / 1.5) as usize;
-        let mut rgb = vec![0u8; height_rgb * image_msg.width as usize * 3];
-        let _ret = unsafe {
-            libyuv::nv12_to_raw(
-                image_msg.data.as_ptr(),
-                image_msg.width as i32,
-                image_msg
-                    .data
-                    .as_ptr()
-                    .add(image_msg.width as usize * height_rgb),
-                image_msg.width as i32,
-                rgb.as_mut_ptr(),
-                image_msg.width as i32 * 3,
-                image_msg.width as i32,
-                height_rgb as i32,
-            )
+        let mut buf: Vec<u8> = vec![];
+        let (height_rgb, rgb) = if image_msg.encoding != "nv12" {
+            (image_msg.height, &image_msg.data)
+        } else {
+            let height_rgb = (image_msg.height as f32 / 1.5) as usize;
+            buf.resize(image_msg.width as usize * height_rgb * 3, 0);
+            let _ret = unsafe {
+                libyuv::nv12_to_raw(
+                    image_msg.data.as_ptr(),
+                    image_msg.width as i32,
+                    image_msg
+                        .data
+                        .as_ptr()
+                        .add(image_msg.width as usize * height_rgb),
+                    image_msg.width as i32,
+                    buf.as_mut_ptr(),
+                    image_msg.width as i32 * 3,
+                    image_msg.width as i32,
+                    height_rgb as i32,
+                )
+            };
+            (height_rgb as u32, &buf)
         };
+
         if let Some(rec) = &self.rec_stream {
             rec.set_timestamp_secs_since_epoch(
                 "main",
                 image_msg.header.stamp.sec as f64 + image_msg.header.stamp.nanosec as f64 * 1e-9,
             );
             rec.log(
-                format!("image/{}", message.channel.topic.clone()),
+                message.channel.topic.clone(),
                 &rerun::Image::from_elements(
-                    rgb.as_ref(),
+                    rgb,
                     [image_msg.width, height_rgb as u32],
                     rerun::ColorModel::RGB,
                 ),
@@ -96,7 +99,7 @@ impl Extractor for Parser {
                     .join(format!("{}.bin", message.publish_time)),
             )?;
             file.write_all(&image_msg.data)?;
-            let img = image::RgbImage::from_vec(image_msg.width, height_rgb as u32, rgb)
+            let img = image::RgbImage::from_vec(image_msg.width, height_rgb as u32, rgb.to_owned())
                 .expect("Image should be valid");
 
             img.save(
