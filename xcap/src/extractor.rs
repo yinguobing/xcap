4bf902f05bb219501053cf342f2b4d521136931f
yinguobing <yinguobing@Gmail.com>
2025-05-20 11:54:36 +0800

    implement default decode function for mcap::Message

--- xcap/src/extractor.rs
+++ xcap/src/extractor.rs
@@ -1,8 +1,18 @@
 use mcap::Message;
 use std::sync::{atomic::AtomicBool, Arc};
+const ZSTD_MAGIC_NUMBER: [u8; 4] = [0x28, 0xb5, 0x2f, 0xfd];
 
 pub trait Extractor {
     type ExtractorError;
+
+    /// Decode the compressed message
+    fn decode(&self, message: &Message) -> Result<Vec<u8>, std::io::Error> {
+        if &message.data[..4] == ZSTD_MAGIC_NUMBER {
+            return zstd::stream::decode_all(message.data.as_ref());
+        } else {
+            return Ok(message.data.to_vec());
+        };
+    }
 
     /// Function to be called for every message.
     fn step(&mut self, message: &Message) -> Result<(), Self::ExtractorError>;
